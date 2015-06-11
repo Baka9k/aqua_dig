5fcfccce7c4c0a12527b7d5bb73f31f9d1fb4335
Baka9k <neoplanspaceliner9@gmail.com>
2015-06-12 00:52:05 +0300

    0

--- script.js
+++ script.js
@@ -23,6 +23,8 @@ function main() {
 	ad.displacementX = 0;
 	ad.displacementY = 0;
 	ad.camera = {};
+	ad.currentFromX = 0;
+	ad.currentFromY = 0;
 	
 	Array.prototype.in_array = function(p_val) {
 		for(var i = 0, l = this.length; i < l; i++)	{
@@ -86,39 +88,42 @@ function main() {
 			ad.draw(stone, x, y, ad.tileWidth, ad.tileHeight);
 		}
 		var probability = 0.01;
-		if ((ap < probability) && (tileY > 30) && (tileY < 50) && (ad.map.in_array(xandy) == false)) {
+		if ((tileY > 30) && (tileY < 50) && (ap < probability) && (ad.map.in_array(xandy) == false)) {
 			ad.draw(quartz, x, y, ad.tileWidth, ad.tileHeight);
 		}
 		probability = 0.03;
-		if ((ap < probability) && (tileY > 51) && (tileY < 50) && (a.in_array(xandy)==false)) {
+		if ((tileY > 51) && (tileY < 100) && (ap < probability) && (ad.map.in_array(xandy)==false)) {
 			ad.draw(emerald, x, y, ad.tileWidth, ad.tileHeight);  
 		}
 	}
 
-	ad.drawWorld = function (fromX, fromY) {		
+	ad.drawWorld = function (fromX, fromY) {
 		for (var i = fromX; i < (fromX + ad.tilesOnX); i++) {
 			for (var j = fromY; j < (fromY + ad.tilesOnY); j++) {
 				ad.drawTile(i, j);
 			}
 		}
-		ad.drawBoat((Math.ceil(ad.tilesOnX / 2) * ad.tileWidth) - ad.tileWidth, 6 * (ad.tileHeight) + ad.tileHeight * 0.2, ad.tileWidth, ad.tileHeight - 11);
-		console.log(fromX,fromX + ad.tilesOnX);
+		ad.drawBoat((Math.ceil(ad.tilesOnX / 2) * ad.tileWidth) - ad.tileWidth, 6 * (ad.tileHeight) + ad.tileHeight * 0.2, ad.tileWidth, ad.tileHeight * 0.725);
+		ad.currentFromX = fromX;
+		ad.currentFromY = fromY;
 	}
 	
 	ad.keypress = function(event) {
 		if (ad.keys == 'enabled') {
 			k = event.keyCode;
-			if (k == 38) {
-				ad.camera.up();
-			}
-			else if (k == 40) {
-				ad.camera.down();
-			}
-			else if (k == 37) {
-				ad.camera.left();
-			}
-			else if (k == 39) {
-				ad.camera.right();
+			switch(k) {
+				case 38:
+					ad.camera.up();
+					break;
+				case 40:
+					ad.camera.down();
+					break;
+				case 37:
+					ad.camera.left();
+					break;
+				case 39:
+					ad.camera.right();
+					break;
 			}
 		}
 		//<	   37
@@ -133,7 +138,6 @@ function main() {
 		var xandy = (tileX + 'and' + tileY);
 		if(!ad.map.in_array(xandy)) {
 			ad.map.push(xandy);
-			console.log(xandy);
 		}
 		
 	}
