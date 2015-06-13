1d806f43173bd5bd890b30dba1e7c169de31dd1a
Baka9k <neoplanspaceliner9@gmail.com>
2015-06-13 23:16:12 +0300

    09

--- script.js
+++ script.js
@@ -17,9 +17,10 @@ function main() {
 	ad.tilesOnX = Math.ceil(ad.width/ad.tileWidth);
 	ad.tilesOnY = Math.ceil(ad.height/ad.tileHeight);
 	ad.lr = 'r';
-	ad.o2 = 100;
+	ad.o2 = 1;
 	ad.keys = 'enabled';
 	ad.map = new Array();
+	ad.currentMap = new Array();
 	ad.displacementX = 0;
 	ad.displacementY = 0;
 	ad.camera = {};
@@ -27,6 +28,13 @@ function main() {
 	ad.currentFromY = 0;
 	ad.currentDisplacementX = 0;
 	ad.currentDisplacementY = 0;
+	ad.items = {
+		quartz: 0,
+		emerald: 0,
+		ruby: 0,
+		diamond: 0
+	};
+	ad.context.font = '16px lucida console';
 	
 	Array.prototype.in_array = function(p_val) {
 		for(var i = 0, l = this.length; i < l; i++)	{
@@ -126,18 +134,26 @@ function main() {
 	document.addEventListener('keydown', ad.keypress, false);
 
 	ad.dig = function (tileX, tileY) {
+		var ap = ad.hash2prob(ad.coordHash(tileX, tileY));
+		var probability = 0.01;
+		if ((tileY > 30) && (tileY < 50) && (ap < probability) && (ad.map.in_array(xandy) == false)) {
+			ad.items.quartz++;
+		}
+		probability = 0.03;
+		if ((tileY > 51) && (tileY < 100) && (ap < probability) && (ad.map.in_array(xandy)==false)) {
+			ad.items.emerald++;
+		}
 		var xandy = (tileX + 'and' + tileY);
 		if(!ad.map.in_array(xandy)) {
 			ad.map.push(xandy);
 		}
-		
 	}
 	
 	ad.camera.right = function () {
 		ad.lr = 'r';
 		ad.keys = "disabled";
 		ad.displacementX++;
-		ad.dig(Math.round(ad.tilesOnX / 2) + ad.displacementX - 1, 6 + ad.displacementY);
+		ad.dig(Math.ceil(ad.tilesOnX / 2) + ad.displacementX - 1, 6 + ad.displacementY);
 		ad.drawWorld(ad.displacementX, ad.displacementY);
 	}
 
@@ -145,14 +161,14 @@ function main() {
 		ad.lr = 'l';
 		ad.keys = "disabled";
 		ad.displacementX--;
-		ad.dig(Math.round(ad.tilesOnX / 2) + ad.displacementX - 1, 6 + ad.displacementY);
+		ad.dig(Math.ceil(ad.tilesOnX / 2) + ad.displacementX - 1, 6 + ad.displacementY);
 		ad.drawWorld(ad.displacementX, ad.displacementY);
 	}
 
 	ad.camera.down = function () {
 		ad.keys = "disabled";
 		ad.displacementY++;
-		ad.dig(Math.round(ad.tilesOnX / 2) + ad.displacementX - 1, 6 + ad.displacementY);
+		ad.dig(Math.ceil(ad.tilesOnX / 2) + ad.displacementX - 1, 6 + ad.displacementY);
 		ad.drawWorld(ad.displacementX, ad.displacementY);
 	}
 	
@@ -160,20 +176,28 @@ function main() {
 		if (ad.displacementY <= 0) return;
 		ad.keys = "disabled";
 		ad.displacementY--;
-		ad.dig(Math.round(ad.tilesOnX / 2) + ad.displacementX - 1, 6 + ad.displacementY);
+		ad.dig(Math.ceil(ad.tilesOnX / 2) + ad.displacementX - 1, 6 + ad.displacementY);
 		ad.drawWorld(ad.displacementX, ad.displacementY);
 	}
 
 	ad.drawWorld = function (fromX, fromY) {
-		var fps = 10;
+		var xandy = ((Math.ceil(ad.tilesOnX / 2) + ad.displacementX - 1) + 'and' + (6 + ad.displacementY)); //d-1
+		var ap = ad.hash2prob(ad.coordHash((Math.ceil(ad.tilesOnX / 2) + ad.displacementX), (6 + ad.displacementY)));
+		if (((!ad.currentMap.in_array(xandy)) && ((fromY + 6) > 21))/* || ((!ad.map.in_array(xandy)) && ((fromY + 6) == 21) && (ap < 0.5))*/) {
+			var t = 1000;
+			var fps = 40;
+		} else {
+			var t = 250;
+			var fps = 10;
+		}
 		var stepX = (ad.currentFromX - fromX) * ad.tileWidth / fps;
 		var stepY = (ad.currentFromY - fromY) * ad.tileHeight / fps;
 		var c = 0;
 		var dx = 0;
 		var dy = 0;
 		var si = setInterval(function() {			
-			for (var i = ad.currentFromX; i < (fromX + ad.tilesOnX); i++) {
-				for (var j = ad.currentFromY; j < (fromY + ad.tilesOnY); j++) {
+			for (var i = ad.currentFromX - 1; i <= (fromX + ad.tilesOnX); i++) {
+				for (var j = ad.currentFromY - 1; j <= (fromY + ad.tilesOnY); j++) {
 					ad.drawTile(i, j, dx, dy);
 				}
 			}
@@ -184,15 +208,34 @@ function main() {
 				clearInterval(si);
 				ad.currentDisplacementX = ad.displacementX;
 				ad.currentDisplacementY = ad.displacementY;
+				ad.currentMap = [];
+				ad.currentMap = ad.map.concat(ad.currentMap);
 				ad.keys = "enabled";
 			}
 			ad.drawBoat((Math.ceil(ad.tilesOnX / 2) * ad.tileWidth) - ad.tileWidth, 6 * (ad.tileHeight) + ad.tileHeight * 0.2, ad.tileWidth, ad.tileHeight * 0.725);
-		}, 250/fps);
+			ad.drawTools();
+		}, t/fps);
 		ad.currentFromX = fromX;
 		ad.currentFromY = fromY;
 	}
 	
+	ad.drawTools = function() {
+		ad.context.fillStyle = "#DDDDDD";
+		ad.context.fillRect(8, 8, 80, 160);
+		ad.draw(quartz, 36, 12, 20, 20);
+		ad.context.fillStyle = "#555555";
+		ad.context.fillText(ad.items.quartz, 65, 28);
+		ad.draw(emerald, 36, 42, 20, 20);
+		ad.context.fillText(ad.items.emerald, 65, 58);
+		ad.draw(ruby, 36, 72, 20, 20);
+		ad.context.fillText(ad.items.ruby, 65, 88);
+		ad.draw(diamond, 36, 102, 20, 20);
+		ad.context.fillText(ad.items.diamond, 65, 118);
+		ad.context.fillStyle = "#00DD00";
+		ad.context.fillRect(15, 15, 10, 130 * ad.o2);
+		ad.context.fillText('O2', 15, 160);
+	}
+	
+	
 	ad.drawWorld(0,0);
-	//ad.drawTools();
-
 }
